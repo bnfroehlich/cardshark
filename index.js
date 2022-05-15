32fae6062aebb574e9ebe0f4624dbf2d4282af3c
Ben Froehlich <bnfroehlich@gmail.com>
2022-05-15 12:33:42 -0400

    initial commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    //res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.send('hello world');
 });
 
 app.get('/', (req, res) => {
