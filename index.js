da40a1342b874eefdb4e6fe52aace917db25e8d8
Ben Froehlich <bnfroehlich@gmail.com>
2022-05-15 12:52:35 -0400

    2

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const app = express();
 const PORT = 3000;
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    //res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-    res.send('hello world');
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    //res.send('hello world');
 });
 
 app.get('/', (req, res) => {
