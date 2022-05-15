59f7fe15117363245bca41958aa13b4e92dbf85c
Ben Froehlich <bnfroehlich@gmail.com>
2022-05-15 13:03:55 -0400

    3

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static(path.resolve(__dirname, 'client/build')));
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
     console.log(__dirname);
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-    //res.send('hello world');
+    //res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.send('hello world');
 });
 
 app.get('/', (req, res) => {
